d7ed5aa7ea6185bad533aeea34331bd05f189633
Ashley Hummel <ashley.l.hummel@gmail.com>
2022-01-30 15:04:27 -0700

    Pie chart (built with plotly) displayed in a web window portraying number of each Wordle score found
    - TODO: menu with user input to view different stats/graphs

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ import plotly.graph_objects as go
 load_dotenv(".env") # private environment variables are located in a local .env file
 
 # Read-only reddit instance
-reddit = praw.Reddit(client_id=os.environ.get('CLIENT_ID'),         # your client id
-                               client_secret=os.environ.get('CLIENT_SECRET'),      # your client secret
-                               user_agent="wordle-scraper")        # your user agent
+reddit = praw.Reddit(client_id=os.environ.get('CLIENT_ID'),                        # your client id
+                              client_secret=os.environ.get('CLIENT_SECRET'),      # your client secret
+                              user_agent="wordle-scraper")                        # your user agent
 
 # subreddit instance
 subreddit = reddit.subreddit("wordle")
@@ -29,7 +29,7 @@ submission = reddit.submission(url=url) # create submission instance from url of
 
 postTitle = submission.title
 
-scoresList = [0,0,0,0,0,0]; # each index represents number of scores found for that score (1/6 - 6/6)
+scoresList = [0,0,0,0,0,0]; # each index+1 represents number of scores found for that score (1/6 - 6/6)
 
 # comment instance
 for top_level_comment in submission.comments:
@@ -39,17 +39,26 @@ for top_level_comment in submission.comments:
   if indexOfScore != -1:
     fullScore = top_level_comment.body[indexOfScore-1:indexOfScore+2]
     score = int(fullScore[0]) # just the number part of the score
-    print(fullScore)
-    print(score)
+    # print(fullScore)
+    # print(score)
     scoresList[score - 1] += 1
-    print(scoresList)
+    # print(scoresList)  
 
-    
-    
+# TODO: MENU
+# User inputs URL of daily wordle
+# View today's stats (mean and mode)
+# Pie chart
+# bar graph
 
-# pie chart
-labels = ['1', '2', '3', '4', '5', '6']
-values = [4500, 2500, 1053, 500] # TODO REPLACE VALUES
+# ---------- pie chart ----------
+labels = ['1/6', '2/6', '3/6', '4/6', '5/6', '6/6']
+values = [scoresList[0], scoresList[1], scoresList[2], scoresList[3], scoresList[4], scoresList[5]]
+
+colors = ['gold', 'mediumturquoise', 'darkorange', 'lightgreen'] # TODO
 
 fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
-# fig.show()
+fig.update_traces(hoverinfo='label+percent', textinfo='value', textfont_size=20,
+                  marker=dict(colors=colors, line=dict(color='#000000', width=2)))
+fig.update_layout(title_text=postTitle)
+
+fig.show()
